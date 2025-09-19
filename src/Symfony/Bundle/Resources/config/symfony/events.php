7770f8a5e96fb532cca8c0fc11efa34af66842b4
LaurentHuzard <laurent@les-tilleuls.coop>
2025-09-19 16:24:56 +0200

    # This is a combination of 2 commits. # This is the 1st commit message:
    feat(state): added "Accept-Post" and "Allow" headers
    
    To ensure compliance with the LDP specification (https://www.w3.org/TR/ldp/):
    Added the "Accept-Post" header containing the list of supported Post formats.
     Added the "Allow" header with values based on the allowed operations on the queried resources.
    
    # Conflicts:
    #	src/Symfony/Bundle/Resources/config/state/processor.xml
    #	src/Symfony/Bundle/Resources/config/symfony/events.xml
    
    # The commit message #2 will be skipped:
    
    # WIP

--- src/Symfony/Bundle/Resources/config/symfony/events.php
+++ src/Symfony/Bundle/Resources/config/symfony/events.php
@@ -90,6 +90,11 @@ return function (ContainerConfigurator $container) {
         ->decorate('api_platform.state_processor.respond', null, 0)
         ->args([service('api_platform.state_processor.add_link_header.inner')]);
 
+
+    $services->set('api_platform.state_processor.linked_data_platform', 'ApiPlatform\State\Processor\LinkedDataPlatformProcessor')
+        ->decorate('api_platform.state_processor.add_link_header', null, 0)
+        ->args([service('api_platform.state_processor.linked_data_platform.inner')]);
+
     $services->set('api_platform.listener.view.write', 'ApiPlatform\Symfony\EventListener\WriteListener')
         ->args([
             service('api_platform.state_processor.write'),
